aee10a0c92eb7092c71fe8e6cb263b1b95f222e6
Maxim <74974283+maximka76667@users.noreply.github.com>
2025-01-20 19:25:33 +0100

    add some styles

--- tc-002/script.js
+++ tc-002/script.js
@@ -1,14 +1,70 @@
 console.log("Hello from JS!");
 
-// Grab the button from the DOM
-const calculateButton = document.getElementById("calculate-button");
+// Buttons
+const calculateSumButton = document.getElementById("calculate-button-sum");
+const calculateRestButton = document.getElementById("calculate-button-rest");
+const calculateMultButton = document.getElementById("calculate-button-mult");
+const calculateDivButton = document.getElementById("calculate-button-div");
 
 // Inputs
-const inputX = document.querySelector(".input__x");
-const inputY = document.querySelector(".input__y");
+const inputX = document.querySelector(".calculator__input_x");
+const inputY = document.querySelector(".calculator__input_y");
 
-// Add a click listener
-calculateButton.addEventListener("click", function () {
-  const result = parseInt(inputX.value) + parseInt(inputY.value);
+const inputResult = document.querySelector(".calculator__input_result");
+
+const inputError = document.querySelector(".calculator__input_error");
+
+// Math actions
+const sum = (a, b) => {
+  return a + b;
+};
+
+const substract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+function isNotNumeric(value) {
+  return isNaN(value) || typeof value === "boolean";
+}
+
+// That's a common function for all math related event listeners
+const calculateAction = (action) => {
+  const x = parseInt(inputX.value); // First input value
+  const y = parseInt(inputY.value); // Second input value
+
+  // If X is non-numeric value show error and prevent action
+  if (isNotNumeric(x)) {
+    inputResult.value = "First input value is invalid";
+    return;
+  }
+
+  // If Y is non-numeric value show error and prevent action
+  if (isNotNumeric(y)) {
+    inputResult.value = "Second input value is invalid";
+    return;
+  }
+
+  const result = action(x, y);
+  inputResult.value = result;
   console.log(`Result: ${result}`);
-});
+};
+
+// Add a sum button click listener
+calculateSumButton.addEventListener("click", () => calculateAction(sum));
+
+// Add a rest button click listener
+calculateRestButton.addEventListener("click", () => calculateAction(substract));
+
+// Add a mult button click listener
+calculateMultButton.addEventListener("click", () => calculateAction(multiply));
+
+// Add a div button click listener
+calculateDivButton.addEventListener("click", () => calculateAction(divide));
